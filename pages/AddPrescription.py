3df0f9577542db7e6e980ed619491043e27eaca1
phantanphuc2512@gmail.com <phantanphuc2512@gmail.com>
2023-06-09 08:32:23 +0700

    Add Add Diagnose function

--- pages/AddPrescription.py
+++ pages/AddPrescription.py
@@ -13,8 +13,12 @@ from tkcalendar import DateEntry
 class AddPrescriptionForm(tk.Frame):
     def __init__(self, parent=None, controller=None):
         tk.Frame.__init__(self, parent)
+        self.entry_width = 100
+        self.current_row = 0
+        self.current_diagnose_count = 0
         self.create_widgets()
         self.controller = controller
+
 
     def create_widgets(self):
         # Create a frame for patient information
@@ -77,48 +81,118 @@ class AddPrescriptionForm(tk.Frame):
 
         # Create a frame for prescription information
 
-        prescription_info_frame = tk.LabelFrame(self, text='Prescription Information')
-        prescription_info_frame.grid(row=0, column=1, padx=10, pady=10)
+        self.prescription_info_frame = tk.LabelFrame(self, text='Prescription Information')
+        self.prescription_info_frame.grid(row=0, column=1, padx=10, pady=10)
 
-        tk.Label(prescription_info_frame, text='Chẩn đoán').grid(row=0, column=0, padx=5, pady=5)
-        chan_doan_entry = tk.Entry(prescription_info_frame, width=50)
-        chan_doan_entry.grid(row=0, column=1, padx=5, pady=5)
+        tk.Label(self.prescription_info_frame, text='Chẩn đoán').grid(row=0, column=0, padx=5, pady=5)
+        # chan_doan_entry = tk.Entry(self.prescription_info_frame, width=50)
+        # chan_doan_entry.grid(row=0, column=1, padx=5, pady=5)
 
-        tk.Label(prescription_info_frame, text='Lưu ý').grid(row=1, column=0, padx=5, pady=5)
-        luu_y_entry = tk.Entry(prescription_info_frame, width=50)
-        luu_y_entry.grid(row=1, column=1, padx=5, pady=5)
+        name_label = tk.Label(self.prescription_info_frame, text="Mã chẩn đoán")
+        name_label.grid(row=0, column=1)
 
-        tk.Label(prescription_info_frame, text='Hình thức điều trị').grid(row=2, column=0, padx=5, pady=5)
-        hinh_thuc_dieu_tri_entry = tk.Entry(prescription_info_frame, width=50)
-        hinh_thuc_dieu_tri_entry.grid(row= 2,column = 1,padx = 5,pady = 5)
+        price_label = tk.Label(self.prescription_info_frame, text="Tên chẩn đoán")
+        price_label.grid(row=0, column=2)
 
-        tk.Label(prescription_info_frame,text='Đợt dùng thuốc').grid(row = 3,column = 0,padx = 5,pady = 5)
-        dot_dung_thuoc_entry = tk.Entry(prescription_info_frame,width = 50)
-        dot_dung_thuoc_entry.grid(row = 3,column = 1,padx = 5,pady = 5)
+        type_label = tk.Label(self.prescription_info_frame, text="Kết luận")
+        type_label.grid(row=0, column=3)
 
-        tk.Label(prescription_info_frame,text='Thông tin đơn thuốc').grid(row = 4,column = 0,padx = 5,pady = 5)
-        thong_tin_don_thuoc_entry = tk.Entry(prescription_info_frame,width = 50)
-        thong_tin_don_thuoc_entry.grid(row = 4,column = 1,padx = 5,pady = 5)
 
-        tk.Label(prescription_info_frame,text='Lời dặn').grid(row = 5,column = 0,padx = 5,pady = 5)
-        loi_dan_entry = tk.Entry(prescription_info_frame,width = 50)
-        loi_dan_entry.grid(row = 5,column = 1,padx = 5,pady = 5)
 
-        tk.Label(prescription_info_frame,text='Số điện thoại người khám bệnh').grid(row=6,column=0,padx=5,pady=5)
-        so_dien_thoai_nguoi_kham_benh_entry=tk.Entry(prescription_info_frame,width=50)
-        so_dien_thoai_nguoi_kham_benh_entry.grid(row=6,column=1,padx=5,pady=5)
+        def add_row():
 
-        tk.Label(prescription_info_frame,text='Ngày tái khám').grid(row=7,column=0,padx=5,pady=5)
-        ngay_tai_kham_entry=DateEntry(prescription_info_frame,width=12,date_pattern='dd/mm/yyyy')
-        ngay_tai_kham_entry.grid(row=7,column=1,padx=5,pady=5)
+            row_thres = 1
 
-        tk.Label(prescription_info_frame,text='Ngày giờ kê đơn').grid(row=8,column=0,padx=5,pady=5)
-        ngay_gio_ke_don_entry=DateEntry(prescription_info_frame,width=12,date_pattern='dd/mm/yyyy')
-        ngay_gio_ke_don_entry.grid(row=8,column=1,padx=5,pady=5)
+            check = self.prescription_info_frame.grid_slaves(column=0)[:-1]
 
-        tk.Label(prescription_info_frame,text='Chữ ký số').grid(row=9,column=0,padx=5,pady=5)
-        chu_ky_so_entry=tk.Entry(prescription_info_frame,width=50)
-        chu_ky_so_entry.grid(row=9,column=1,padx=5,pady=5)
+            for widget in self.prescription_info_frame.grid_slaves():
+                if widget.grid_info()['row'] >= row_thres + self.current_diagnose_count:
+                    widget.grid(row=widget.grid_info()['row'] + 1, column=widget.grid_info()['column'])
+
+
+            name_entry = tk.Entry(self.prescription_info_frame)
+            name_entry.grid(row=row_thres + self.current_diagnose_count, column=1)
+
+            # Add price field
+            price_entry = tk.Entry(self.prescription_info_frame)
+            price_entry.grid(row=row_thres + self.current_diagnose_count, column=2)
+
+            # Add type field
+            type_entry = tk.Entry(self.prescription_info_frame)
+            type_entry.grid(row=row_thres + self.current_diagnose_count, column=3)
+
+            self.current_diagnose_count = self.current_diagnose_count + 1
+
+            # self.current_row += 1
+            # [x.grid(row=x.grid_info()['row'] + 1, column=0)
+            #  for x in self.prescription_info_frame.grid_slaves(column=0)[:-1]]
+
+            # [x.grid(row=x.grid_info()['row'] + 1, column=2)
+            #  for x in self.prescription_info_frame.grid_slaves(column=2)[:-1]]
+
+
+
+        add_button = tk.Button(self.prescription_info_frame, text="Add Medicine", command=add_row)
+        add_button.grid(row=1, column=2)
+
+        # Create button to remove last row
+        remove_button = tk.Button(self.prescription_info_frame, text="Remove Medicine")
+        remove_button.grid(row=1, column=3)
+
+        self.current_row = 2
+
+
+        tk.Label(self.prescription_info_frame, text='Lưu ý').grid(row=self.current_row, column=0, padx=5, pady=5)
+        luu_y_entry = tk.Entry(self.prescription_info_frame, width=self.entry_width)
+        luu_y_entry.grid(row=self.current_row, column=1, columnspan=3, padx=5, pady=5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame, text='Hình thức điều trị').grid(row=self.current_row, column=0, padx=5, pady=5)
+        hinh_thuc_dieu_tri_entry = tk.Entry(self.prescription_info_frame, width=self.entry_width)
+        hinh_thuc_dieu_tri_entry.grid(row= self.current_row,column = 1, columnspan=3,padx = 5,pady = 5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Đợt dùng thuốc').grid(row = self.current_row,column = 0,padx = 5,pady = 5)
+        dot_dung_thuoc_entry = tk.Entry(self.prescription_info_frame,width = self.entry_width)
+        dot_dung_thuoc_entry.grid(row = self.current_row,column = 1, columnspan=3,padx = 5,pady = 5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Thông tin đơn thuốc').grid(row = self.current_row,column = 0,padx = 5,pady = 5)
+        thong_tin_don_thuoc_entry = tk.Entry(self.prescription_info_frame,width = self.entry_width)
+        thong_tin_don_thuoc_entry.grid(row = self.current_row,column = 1, columnspan=3,padx = 5,pady = 5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Lời dặn').grid(row = self.current_row,column = 0,padx = 5,pady = 5)
+        loi_dan_entry = tk.Entry(self.prescription_info_frame,width = self.entry_width)
+        loi_dan_entry.grid(row = self.current_row,column = 1, columnspan=3,padx = 5,pady = 5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Số điện thoại người khám bệnh').grid(row=self.current_row,column=0,padx=5,pady=5)
+        so_dien_thoai_nguoi_kham_benh_entry=tk.Entry(self.prescription_info_frame,width=self.entry_width)
+        so_dien_thoai_nguoi_kham_benh_entry.grid(row=self.current_row,column=1, columnspan=3,padx=5,pady=5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Ngày tái khám').grid(row=self.current_row,column=0,padx=5,pady=5)
+        ngay_tai_kham_entry=DateEntry(self.prescription_info_frame,width=12,date_pattern='dd/mm/yyyy')
+        ngay_tai_kham_entry.grid(row=self.current_row,column=1,padx=5,pady=5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Ngày giờ kê đơn').grid(row=self.current_row,column=0,padx=5,pady=5)
+        ngay_gio_ke_don_entry=DateEntry(self.prescription_info_frame,width=12,date_pattern='dd/mm/yyyy')
+        ngay_gio_ke_don_entry.grid(row=self.current_row,column=1,padx=5,pady=5)
+
+        self.current_row = self.current_row + 1
+
+        tk.Label(self.prescription_info_frame,text='Chữ ký số').grid(row=self.current_row,column=0,padx=5,pady=5)
+        chu_ky_so_entry=tk.Entry(self.prescription_info_frame,width=self.entry_width)
+        chu_ky_so_entry.grid(row=self.current_row,column=1, columnspan=3,padx=5,pady=5)
 
 
 
